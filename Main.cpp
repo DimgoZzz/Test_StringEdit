38dd996d1cc39a36312f074e5d5c85d85a0bfcbf
Dmitry <dmgolovinzzz@gmail.com>
2021-12-08 17:30:48 +0300

    done

--- Main.cpp
+++ Main.cpp
@@ -16,59 +16,61 @@ void lowerstring(string& str)
 
 int main()
 {
-	
-	cout << "Enter string: \n";
-
-	string str;
-	getline(cin, str);
-
-	int strlen = str.length();
-
-	if (strlen == 0) { cout << "String Is Empty!\n";  return 0; }
-
-	lowerstring(str);
-
-	vector<tuple<char, bool,vector<int>>> chars; 
-	chars.reserve(strlen);
-	
-	for (int i1 = 0; i1 < strlen; ++i1)
+	while (true)
 	{
-		char currchar = str[i1];
+		cout << "Enter string: \n";
 
-		int vecsize = chars.size();
-		bool finded = false;
-		for (int i2 = 0; i2 < vecsize; ++i2)
+		string str;
+		getline(cin, str);
+
+		int strlen = str.length();
+
+		if (strlen == 0) { cout << "String Is Empty!\n";  return 0; }
+
+		lowerstring(str);
+
+		vector<tuple<char, bool, vector<int>>> chars;
+		chars.reserve(strlen);
+
+		for (int i1 = 0; i1 < strlen; ++i1)
 		{
-			if (get<0>(chars[i2]) == currchar)
+			char currchar = str[i1];
+
+			int vecsize = chars.size();
+			bool finded = false;
+			for (int i2 = 0; i2 < vecsize; ++i2)
 			{
-				get<1>(chars[i2]) = true;
-				get<2>(chars[i2]).push_back(i1);
-				finded = true;
-				continue;
+				if (get<0>(chars[i2]) == currchar)
+				{
+					get<1>(chars[i2]) = true;
+					get<2>(chars[i2]).push_back(i1);
+					finded = true;
+					continue;
+				}
+			}
+			if (finded) { continue; }
+
+			vector<int> arr; arr.push_back(i1);
+			chars.push_back(tuple<char, bool, vector<int>>(currchar, false, arr));
+		}
+
+		string result;
+		result.resize(strlen);
+
+		int chararrsize = chars.size();
+		for (int i1 = 0; i1 < chararrsize; ++i1)
+		{
+			char ch = get<1>(chars[i1]) ? ')' : '(';
+
+			vector<int>& positions = get<2>(chars[i1]);
+
+			int possize = positions.size();
+			for (int i2 = 0; i2 < possize; ++i2)
+			{
+				result[positions[i2]] = ch;
 			}
 		}
-		if (finded) { continue; }
 
-		vector<int> arr; arr.push_back(i1);
-		chars.push_back(tuple<char, bool, vector<int>>(currchar,false,arr));
+		cout << result << endl;
 	}
-
-	string result; 
-	result.resize(strlen);
-
-	int chararrsize = chars.size();
-	for (int i1 = 0; i1 < chararrsize; ++i1)
-	{
-		char ch = get<1>(chars[i1]) ? ')' : '(';
-
-		vector<int>& positions = get<2>(chars[i1]);
-		
-		int possize = positions.size();
-		for (int i2 = 0; i2 < possize; ++i2)
-		{
-			result[positions[i2]] = ch;
-		}
-	}
-
-	cout << result << endl;
 }
